e0135d9f88287ac755715d6af00013b4cf51a4e1
Lau-951 <73627167+Lau-951@users.noreply.github.com>
2020-10-30 11:44:55 -0600

    Documentación codigo
    Se le agrego la documentacion necesaria a comprender el funcionamiento del codigo.

--- Paint.py
+++ Paint.py
@@ -9,10 +9,18 @@ Exercises
 5. Add width parameter.
 
 """
+"""Se crea una ventana la cual simula un canvas, donde se puede dibujar
+   lineas, cuadrados, rectangulos, triangulos y circulos de diferentes
+   colores.
+"""
 
 from turtle import *
 from freegames import vector
 
+"""Se define la funcion line, la cual recibe dos coordenadas
+   como parametro. La funcion dibuja una linea apartir de la
+   primera coordenada hasta la segunda.
+"""
 def line(start, end):
     "Draw line from start to end."
     up()
@@ -20,6 +28,11 @@ def line(start, end):
     down()
     goto(end.x, end.y)
 
+"""Se define la funcion square, la cual recibe dos coordenada,
+   las cuales va a equivaler el tamaño de los lados del cuadrado.
+   Dentro de un ciclo for se dibuja el lado y se cambia la direccion
+   del cursor 90 grados, esto se ejecuta cuatro veces.
+"""    
 def square(start, end):
     "Draw square from start to end."
     up()
@@ -35,7 +48,7 @@ def square(start, end):
 
 """Se define la funcion circulo, la cual recibe dos coordenada
    para formar la figura de un circulo, siendo la distancia de
-   estos dos puntos el tamaño de la circunferencia del circulo
+   estos dos puntos el tamaño de la circunferencia del circulo.
 """
 def circle(start, end):
     "Draw circle from start to end."
@@ -57,6 +70,13 @@ def circle(start, end):
     left(265) #Se regresa el cursor a su direccion original
     end_fill()
 
+ """Se define la funcion rectangle, la cual recibe dos coordenadas,
+    con las cuales se define la el tamaño de la base y la altura de
+    un rectangulo. Desntro de un for se dibuja primero la base, se
+    cambia la direccion del cursor 90 grados y se dibuja la altura
+    para despues volver a cambiar de direccion 90 grados, esto se 
+    repite dos veces.
+ """   
 def rectangle(start, end):
     "Draw rectangle from start to end."
     up()
@@ -65,13 +85,18 @@ def rectangle(start, end):
     begin_fill()
 
     for count in range(2):
-        forward(end.x - start.x)
+        forward(end.x - start.x) #Se dibuja la base
         left(90)
-        forward(end.y - start.y)
+        forward(end.y - start.y) #Se dibuja la altura
         left(90)
 
     end_fill()
 
+"""Se define la funcion triangle, la cual recibe dos coordenadas
+   como parametro, las cuales definen el tamaño de los lados del
+   triangulo. Dentro de un ciclo for, se dibuja un lado del 
+   triangulo y se cambia la direccion 120 grados.
+"""
 def triangle(start, end):
     "Draw triangle from start to end."
     up()
@@ -85,6 +110,12 @@ def triangle(start, end):
        
     end_fill()
 
+"""Se define la funcion tap, la cual recibe los valores de x e y
+   de una coordenada. Cuando no hay ningun valor como la
+   coordenada inicial, se le asigna estos valores, si la
+   coordenada inicial ya tiene valores, se le asiganan los valores
+   dados como parametro a la coordenada final.
+"""
 def tap(x, y):
     "Store starting point or draw shape."
     start = state['start']
@@ -97,10 +128,16 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
+"""Se define la funcion store, la cual asigan la funcion de alguna
+   figura shape.
+"""
 def store(key, value):
     "Store value in state at key."
     state[key] = value
 
+"""Se inicializan los valores y se definen teclas a pulsar para elegir
+   los coleres y las figuras.
+"""
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
 onscreenclick(tap)
